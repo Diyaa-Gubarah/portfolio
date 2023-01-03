e16ab0d48f935b96eb1ee8fb932b7f1a86bbd39e
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-01-04 01:17:01 +0400

    update message format space.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -8,7 +8,7 @@ export const concatenate = (array: { [key: string]: any | string }[]): string =>
     return array.reduce((acc, item) => {
         const key = Object.keys(item)[0];
         const value = item[key];
-        return `${acc}\n*${key}* _${value}_%0a`;
+        return `${acc}\n${key} *${value}*%0a%0a%0a`;
     }, '');
 }
 
