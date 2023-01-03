e2a4dfb7beab5ff715e7eedc6dc76e66201e4961
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-01-04 01:14:26 +0400

    update message format.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -8,7 +8,7 @@ export const concatenate = (array: { [key: string]: any | string }[]): string =>
     return array.reduce((acc, item) => {
         const key = Object.keys(item)[0];
         const value = item[key];
-        return `${acc}\n*${key}* _${value}_`;
+        return `${acc}\n*${key}* _${value}_%0a`;
     }, '');
 }
 
