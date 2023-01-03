ac4bce0e5ddc5492ed08480b6b2e0a67a82468b1
Diyaa <36938682+Diyaa-Gubarah@users.noreply.github.com>
2023-01-04 01:23:20 +0400

    update message format.

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -5,10 +5,10 @@ export const keyExists = (array: { [key: string]: any }[], key: string): boolean
 }
 
 export const concatenate = (array: { [key: string]: any | string }[]): string => {
-    return array.reduce((acc, item, i) => {
+    return array.reduce((acc, item,) => {
         const key = Object.keys(item)[0];
         const value = item[key];
-        return `${i}. ${acc}\n${key} *${value}*%0a%0a%0a`;
+        return `${acc}\n${key} *${value}*%0a%0a%0a`;
     }, '');
 }
 
